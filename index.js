799fe0f486de9efc982b2782844384441ae5d462
Dmitry Lysenko <demansn@gmail.com>
2016-05-09 12:18:35 +0300

    Реализован прототип AJAX

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('www'));
+
+app.use(bodyParser.json());
+
+app.post('/connect', function(req, res){
+
+	console.log('connected', req.body.id);
+
+	res.send('connected - ' + req.body.id);
+});
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
